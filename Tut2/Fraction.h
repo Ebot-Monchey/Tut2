247d665f647091900dd7a0aca8884162f65a438b
Ebot-Monchey <ebot.ojong@gmail.com>
2015-03-03 14:59:13 +0200

    Defined initial Fraction class structure

---
+++ Tut2/Fraction.h
@@ -0,0 +1,18 @@
+#ifndef FRACTION_H
+#define FRACTION_H
+	class Fraction {
+	private:
+		int numer;		// The numerator of the fraction
+		int denom;		// The denominator of the fraction
+
+	public:
+		Fraction();
+		~Fraction();
+
+		Fraction add(Fraction other);
+		Fraction subtract(Fraction other);
+		Fraction multiply(Fraction other);
+		Fraction divide(Fraction other);
+		void print();
+	};
+#endif
